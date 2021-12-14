762f2ec58a7d11b0e0ae28bc00f73292fb2db6a9
sveta6 <74709261+sveta6@users.noreply.github.com>
2021-12-14 23:57:51 +0300

    doExtraTask

---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="create-todo">
+    <input v-model="name" type="text" placeholder="Название дела" />
+    <button @click="onCreateTodoClicked">Создать</button>
+  </section> 
+</template>
+
+<script>
+import { createTodo } from "@/netClient/todoService";
+export default {
+  name: "CreateTodo",
+  data: () => ({
+    name: "",
+  }),
+  methods: {
+    async onCreateTodoClicked() {
+      try {
+        const newTodo = await createTodo({ title: this.name });
+        if (newTodo) {
+          this.name = "";
+        }
+        this.$emit("todo-created");
+      } catch (error) {
+        console.error({ error });
+      }
+    },
+  },
+};
+</script>
